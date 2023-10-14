81c27739f5140475fd6d82d045a7f158fc59153c
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-10-14 03:38:09 -0400

    modified git execution

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 git status
+
+echo "🤖 What do you want to track using $(git add ...) ⤵️"
+read -r files
+git add "$files"
 echo "🤖 Enter Commit Message: ⤵️"
 read -r message
-git add :/
-git commit -am "$message"
+git commit -m "$message"
 git push
 # git remote add origin git@github.com:imflash217/crafting_interpreters.git
 # git branch -M main
