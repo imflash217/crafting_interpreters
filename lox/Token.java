895740f6845e318d204650e41b5e4fb403ee4c45
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-10-14 03:39:54 -0400

    Token Class

---
+++ lox/Token.java
@@ -0,0 +1,22 @@
+package lox;
+
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        // class constructor
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return String.format("%s %s %s", type, lexeme, literal);
+        // return type + " " + lexeme + " " + literal;
+    }
+
+}
