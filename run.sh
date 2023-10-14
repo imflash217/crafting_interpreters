8f871ff734af7456259d29b92687ec1610fcdf80
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-10-14 02:01:56 -0400

    adding runner script

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-git remote add origin git@github.com:imflash217/crafting_interpreters.git
-git branch -M main
-git push -u origin main
+git status
+echo "🤖 Enter Commit Message: ⤵️"
+read -r message
+git commit -am "$message"
+git push
+# git remote add origin git@github.com:imflash217/crafting_interpreters.git
+# git branch -M main
+# git push -u origin main
