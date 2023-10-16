367b46e454627f251dddcf94a5e485e3bb868c44
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-10-16 05:47:29 -0400

    Scanner class

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -5,6 +5,31 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static lox.TokenType.*;
+
 public class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0; // points at the first character to be scanned in the lexeme
+    private int current = 0; // points at the character in lexeme that is currently being scanned
+    private int line = 1; // points at the line in the source where the scanner is
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd) {
+            // We are at the beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 
 }
