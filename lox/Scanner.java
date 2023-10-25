9d134530b75f19adae386f676ec129555368e685
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2023-10-25 03:04:22 -0400

    single character and two character lexemes

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -14,6 +14,7 @@ public class Scanner {
     private int current = 0; // points at the character in lexeme that is currently being scanned
     private int line = 1; // points at the line in the source where the scanner is
 
+    // constructor
     Scanner(String source) {
         this.source = source;
     }
@@ -24,12 +25,87 @@ public class Scanner {
             start = current;
             scanToken();
         }
+
+        // add a "end-of-file" EOF token at the end of all tokens
         tokens.add(new Token(EOF, "", null, line));
         return tokens;
     }
 
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+            default:
+                Lox.error(line, "Unexpected Character");
+        }
+    }
+
     private boolean isAtEnd() {
         return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd())
+            return false;
+        if (source.charAt(current) != expected)
+            return false;
+
+        current++;
+        return true;
     }
 
 }
